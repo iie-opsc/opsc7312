7d26da6f930a742e0c6e6d6260e3f0f2abba8aac
Hermien Pellissier <miena.moo@gmail.com>
2020-06-25 15:39:28 +0200

    Added getResponseFromHttpUrl method to NetworkUtil

--- LearningUnit1/BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/NetworkUtil.java
+++ LearningUnit1/BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/NetworkUtil.java
@@ -3,8 +3,12 @@ package za.ac.iie.opsc.basicweatherapp;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Utility class that handles the network connections to AccuWeather.
@@ -21,8 +25,11 @@ public class NetworkUtil {
     private NetworkUtil() {
     }
 
+    /**
+     * Creates the URL to get the 5-day forecast for Durban, in metric units of measurement.
+     * @return The URL for the service.
+     */
     public static URL buildURLForWeather() {
-
         Uri buildUri = Uri.parse(WEATHERBASE_URL).buildUpon()
                 .appendQueryParameter(PARAM_API_KEY,
                         BuildConfig.ACCUWEATHER_API_KEY) // passing in api key
@@ -41,5 +48,31 @@ public class NetworkUtil {
         Log.i(LOGGING_TAG, "buildURLForWeather: " + url);
 
         return url;
+    }
+
+    /**
+     * Establishes a connection to a web service, and reads the response from the server.
+     * @param url The URL of the web service to call.
+     * @return The full results string returned by the web service.
+     * @throws IOException Thrown when the URL cannot be connected to.
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection =
+                (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in  = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("//A");
+            boolean hasInput = scanner.hasNext();
+
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
