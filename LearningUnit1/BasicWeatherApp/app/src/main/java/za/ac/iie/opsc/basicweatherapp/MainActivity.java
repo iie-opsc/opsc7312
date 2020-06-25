dbd66e19066a02e59587e4469ed9dfe97a4409d8
Hermien Pellissier <miena.moo@gmail.com>
2020-06-25 16:19:20 +0200

    Module Manual 1.6.4: Requesting Data from the AccuWeather Service

--- LearningUnit1/BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.java
+++ LearningUnit1/BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.java
@@ -2,13 +2,53 @@ package za.ac.iie.opsc.basicweatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvWeather = findViewById(R.id.tv_weather);
+
+        URL url = NetworkUtil.buildURLForWeather();
+        new FetchWeatherData().execute(url);
+    }
+
+    /**
+     * Asynchronous task that requests weather data.
+     */
+    class FetchWeatherData extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL weatherURL = urls[0];
+            String weatherData = null;
+            try {
+                weatherData =
+                        NetworkUtil.getResponseFromHttpUrl(weatherURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return weatherData;
+        }
+
+        @Override
+        protected void onPostExecute(String weatherData) {
+            if (weatherData != null) {
+                tvWeather.setText(weatherData);
+            }
+            super.onPostExecute(weatherData);
+        }
     }
 }
