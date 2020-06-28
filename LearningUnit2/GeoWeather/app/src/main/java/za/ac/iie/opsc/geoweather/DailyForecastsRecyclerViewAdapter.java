48bf2b12543eda91fe2fd93dc94a384679b052c9
Hermien Pellissier <miena.moo@outlook.com>
2020-06-28 17:13:10 +0200

    Copied LU1 files and added launcher icon

---
+++ LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/DailyForecastsRecyclerViewAdapter.java
@@ -0,0 +1,70 @@
+package za.ac.iie.opsc.geoweather;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import za.ac.iie.opsc.geoweather.model.DailyForecasts;
+
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link za.ac.iie.opsc.geoweather.model.DailyForecasts}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class DailyForecastsRecyclerViewAdapter extends RecyclerView.Adapter<DailyForecastsRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DailyForecasts> mValues;
+
+    public DailyForecastsRecyclerViewAdapter(List<DailyForecasts> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        DailyForecasts item = mValues.get(position);
+        holder.mItem = item;
+        holder.tvDate.setText(item.getDate().substring(0, 10));
+        holder.tvMinimum.setText(item.getTemperature().getMinimum().getValue() +
+                        " " + item.getTemperature().getMinimum().getUnit());
+        holder.tvMaximum.setText(item.getTemperature().getMaximum().getValue() +
+                " " + item.getTemperature().getMaximum().getUnit());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView tvDate;
+        public final TextView tvMinimum;
+        public final TextView tvMaximum;
+        public DailyForecasts mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            tvDate = (TextView) view.findViewById(R.id.tv_date);
+            tvMinimum = (TextView) view.findViewById(R.id.tv_minimum);
+            tvMaximum = (TextView) view.findViewById(R.id.tv_maximum);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + tvDate.getText() + "'";
+        }
+    }
+}
---
+++ LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/NetworkUtil.java
@@ -0,0 +1,78 @@
+package za.ac.iie.opsc.geoweather;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Utility class that handles the network connections to AccuWeather.
+ */
+public class NetworkUtil {
+
+    private static final String WEATHERBASE_URL =
+        "https://dataservice.accuweather.com/forecasts/v1/daily/5day/305605";
+    private static final String PARAM_METRIC ="metric";
+    private static final String METRIC_VALUE = "true";
+    private static final String PARAM_API_KEY = "apikey";
+    private static final String LOGGING_TAG = "URLWECREATED";
+
+    private NetworkUtil() {
+    }
+
+    /**
+     * Creates the URL to get the 5-day forecast for Durban, in metric units of measurement.
+     * @return The URL for the service.
+     */
+    public static URL buildURLForWeather() {
+        Uri buildUri = Uri.parse(WEATHERBASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY,
+                        BuildConfig.ACCUWEATHER_API_KEY) // passing in api key
+                .appendQueryParameter(PARAM_METRIC,
+                        METRIC_VALUE) // passing in metric as measurement unit
+                .build();
+
+        URL url = null;
+
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.i(LOGGING_TAG, "buildURLForWeather: " + url);
+
+        return url;
+    }
+
+    /**
+     * Establishes a connection to a web service, and reads the response from the server.
+     * @param url The URL of the web service to call.
+     * @return The full results string returned by the web service.
+     * @throws IOException Thrown when the URL cannot be connected to.
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection =
+                (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in  = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("//A");
+            boolean hasInput = scanner.hasNext();
+
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
