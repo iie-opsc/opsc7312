cffba725611f72f47ab09230fdf018aff72fbc13
Hermien Pellissier <miena.moo@outlook.com>
2020-06-28 21:42:42 +0200

    Module Manual 2.2: Requesting permissions and getting location

--- LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/MainActivity.java
+++ LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/MainActivity.java
@@ -1,39 +1,133 @@
 package za.ac.iie.opsc.geoweather;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import za.ac.iie.opsc.geoweather.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+
+        fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(this);
+        checkPermissionsAndRequestLocation();
+
         FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action",
+                        Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void checkPermissionsAndRequestLocation() {
+        int hasFineLocationPermission = ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCourseLocationPermission = ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED &&
+                hasCourseLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            final String [] permissions = {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION};
+            // Request permission - this is asynchronous
+            ActivityCompat.requestPermissions(this, permissions, 0);
+        }
+        else {
+            // We have permission, so now ask for the location
+            getLocationAndCreateUI();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // is this for our request?
+        if (requestCode == 0) {
+            if (grantResults.length > 0 &&
+                    (grantResults[0] == PackageManager.PERMISSION_GRANTED ||
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                getLocationAndCreateUI();
+            }
+            else {
+                Toast.makeText(MainActivity.this,
+                        "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocationAndCreateUI() {
+        LocationRequest locationRequest = buildLocationRequest();
+        LocationCallback locationCallback = buildLocationCallBack();
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                locationCallback, Looper.myLooper());
+    }
+
+    private LocationRequest buildLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setSmallestDisplacement(10.0f);
+        return locationRequest;
+    }
+
+    private LocationCallback buildLocationCallBack() {
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.i("LocationResult", "onLocationResult: " +
+                        locationResult.getLastLocation());
+
+                // TODO: Get the locationKey from AccuWeater
+
+                SectionsPagerAdapter sectionsPagerAdapter =
+                        new SectionsPagerAdapter(
+                                MainActivity.this,
+                                getSupportFragmentManager());
+                ViewPager viewPager = findViewById(R.id.view_pager);
+                viewPager.setAdapter(sectionsPagerAdapter);
+                TabLayout tabs = findViewById(R.id.tabs);
+                tabs.setupWithViewPager(viewPager);
+            }
+        };
+        return locationCallback;
     }
 }
