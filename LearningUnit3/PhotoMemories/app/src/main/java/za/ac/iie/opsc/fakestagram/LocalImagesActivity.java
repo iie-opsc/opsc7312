baaefea31ef51511832e80ee80666ba68363b36d
Hermien Pellissier <miena.moo@outlook.com>
2020-07-04 12:15:55 +0200

    Creating the store images fragment components

--- LearningUnit3/PhotoMemories/app/src/main/java/za/ac/iie/opsc/fakestagram/LocalImagesActivity.java
+++ LearningUnit3/PhotoMemories/app/src/main/java/za/ac/iie/opsc/fakestagram/LocalImagesActivity.java
@@ -1,14 +1,33 @@
 package za.ac.iie.opsc.fakestagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LocalImagesActivity extends AppCompatActivity {
+
+    private LocalImagesStoreFragment storeFragment =
+            new LocalImagesStoreFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_images);
+
+        Button addButton = findViewById(R.id.btn_add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                transaction.replace(R.id.local_image_place_holder, storeFragment);
+                transaction.commitAllowingStateLoss();
+            }
+        });
     }
 }
